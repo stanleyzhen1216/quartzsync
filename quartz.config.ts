78300ef7e80c96809b05b124c0c62d4edec9803e
stanleyzhen1216 <y.cheng@hey.com>
2026-04-19 00:24:11 +0800

    Update font settings in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+          header: "Inter",
+          body: "Newsreader",
+          code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
